a042650c26402c4e6bc2f3957848dfd3b5112050
SGenvier <132619559+SGenvier@users.noreply.github.com>
2025-06-24 15:55:25 +0100

    Add .gitignore to exclude database files

--- db/database.py
+++ db/database.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
-DATABASE_URL = "sqlite:///./registo_tempos.db"
+#DATABASE_URL = "sqlite:///./registo_tempos.db"
+db_path = os.environ.get("DB_PATH", "/tmp/tempo_registo.db")
+engine = create_engine(f"sqlite:///{db_path}")
 
 #if os.path.exists("registo_tempos.db"):
 #    
 #    os.remove("registo_tempos.db")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
